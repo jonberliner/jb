5d10ba3e27086418bca3bc88f9c87f12935039c6
Jon Berliner <j.s.berliner@gmail.com>
2017-05-13 18:32:09 -0700

    batch norm and dropout added

--- modules_class.py
+++ modules_class.py
@@ -2,6 +2,20 @@ import tensorflow as tf
 from tensorflow.contrib.distributions import Normal, kl
 import numpy as np
 
+def ph(shape, dtype=tf.float32, name=None):
+    return tf.placeholder(dtype=dtype, shape=shape, name=name)
+
+
+def rank(x):
+    return len(x.get_shape())
+
+
+def static_size(x, d):
+    out = x.get_shape()[d].value
+    assert out is not None, 'shape of dim d is not static'
+    return out
+
+
 class Module(object):
     def __init__(self, name=None):
         self.name = name
@@ -58,3 +72,67 @@ class Sequential(object):
         self.layers.append(layer)
 
 
+class BatchNorm(Module):
+    def __init__(self, beta_trainable=True, gamma_trainable=True, name='BatchNorm'):
+        """
+            n_out:       integer, depth of input maps
+            input_rank:  rank of input x
+            phase_train: boolean tf.Varialbe, true indicates training phase
+            name:       string, variable scope
+        Return:
+            normed:      batch-normalized maps
+        """
+        self.beta_trainable = beta_trainable
+        self.gamma_trainable = gamma_trainable
+        super(BatchNorm, self).__init__()
+
+    def __call__(self, x, train_flag):
+        with tf.name_scope(self.name) as scope:
+            if self.called:
+                assert rank(x) == self.input_rank
+                assert static_size(x, -1) == self.input_size[-1]
+            else:
+                self.input_rank = rank(x)
+                self.input_size = x.get_shape()
+                self.output_size = x.get_shape()
+                self.pool_axes = np.arange(self.input_rank-1).tolist()
+                self.n_out = static_size(x, -1)
+                self.beta = tf.Variable(tf.constant(0.0, shape=[self.n_out]),
+                                            name='beta', trainable=self.beta_trainable)
+                self.gamma = tf.Variable(tf.constant(1.0, shape=[self.n_out]),
+                                            name='gamma', trainable=self.gamma_trainable)
+
+            batch_mean, batch_var = tf.nn.moments(x, self.pool_axes, name='moments')
+            ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+            def mean_var_with_update():
+                ema_apply_op = ema.apply([batch_mean, batch_var])
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(batch_mean), tf.identity(batch_var)
+
+            mean, var = tf.cond(train_flag,
+                                mean_var_with_update,
+                                lambda: (ema.average(batch_mean), ema.average(batch_var)))
+            normed = tf.nn.batch_normalization(x, mean, var, self.beta, self.gamma, 1e-3)
+
+            normed.batch_mean = batch_mean
+            normed.batch_var = batch_var
+            normed.mean = mean
+            normed.var = var
+            super(BatchNorm, self).__call__()
+            return normed
+
+
+class Dropout(Module):
+    def __init__(self, p_keep):
+        self.p_keep = p_keep
+        super(Dropout, self).__init__()
+
+    def __call__(self, x, train_flag):
+        mask = lambda: tf.to_float(tf.random_uniform(tf.shape(x)) < self.p_keep)
+        # lambda: (x * mask) / self.p_keep,
+        dropped = tf.cond(train_flag, 
+                          lambda: (x * mask()) / self.p_keep,
+                          lambda: x)
+        super(Dropout, self).__call__()
+        return dropped
