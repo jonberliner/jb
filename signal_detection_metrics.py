d2be98ba15387cd6c1cd07948717c46908cb0e85
Jon Berliner <jsberliner@gmail.com>
2017-06-22 22:20:19 +0000

    fixed and working (only spec tested so far)

--- signal_detection_metrics.py
+++ signal_detection_metrics.py
@@ -12,45 +12,54 @@ def yhat_positive(logits=None, probs=None, crit=0.5):
 def bool_correct(yhat, y):
     y = tf.cast(y, tf.bool)
     yhat = tf.cast(yhat, tf.bool)
-    correct = tf.equal(yhat_positive, y)
+    correct = tf.equal(yhat, y)
     return correct
 
 
 def is_true_positive(yhat, y):
+    y = tf.cast(y, tf.bool)
     correct = bool_correct(yhat, y)
     return tf.logical_and(correct, y)
 
 
 def is_false_positive(yhat, y):
+    y = tf.cast(y, tf.bool)
     incorrect = tf.logical_not(bool_correct(yhat, y))
     return tf.logical_and(incorrect, y)
 
 
 def is_true_negative(yhat, y):
+    y = tf.cast(y, tf.bool)
     correct = bool_correct(yhat, y)
     return tf.logical_and(correct, tf.logical_not(y))
 
 
 def is_false_negative(yhat, y):
+    y = tf.cast(y, tf.bool)
     incorrect = tf.logical_not(bool_correct(yhat, y))
     return tf.logical_and(incorrect, tf.logical_not(y))
 
 
 def sensitivity(is_true_positives, y):
-    ntp = tf.to_float(tf.reduce_sum(is_true_positives))
-    np = tf.to_floa(tf.reduce_sum(y))
+    y = tf.cast(y, tf.bool)
+    ntp = tf.reduce_sum(tf.to_float(is_true_positives))
+    np = tf.reduce_sum(tf.to_float(y))
     return ntp / np
 
 
 def specificity(is_true_negatives, y):
-    ntn = tf.to_float(tf.reduce_sum(is_true_negatives))
-    nn = tf.to_float(tf.reduce_sum(tf.logical_not(y)))
+    y = tf.cast(y, tf.bool)
+    is_true_negatives = tf.to_float(is_true_negatives)
+    ntn = tf.reduce_sum(is_true_negatives)
+    nn = tf.reduce_sum(tf.to_float(tf.logical_not(y)))
     return ntn / nn
 
 
 def precision(is_true_positives, is_false_positives):
-    ntp = tf.to_float(tf.reduce_sum(is_true_positives))
-    nfp = tf.to_float(tf.reduce_sum(is_false_negatives))
+    is_true_positives = tf.to_float(is_true_positives)
+    is_false_positives = tf.to_float(is_false_positives)
+    ntp = tf.reduce_sum(is_true_positives)
+    nfp = tf.reduce_sum(is_false_negatives)
     return ntp / (ntp + nfp)
 
 
@@ -62,14 +71,14 @@ def sens(logits=None, probs=None, targets=None, crit=0.5):
 
 def spec(logits=None, probs=None, targets=None, crit=0.5):
     yhat = yhat_positive(logits=logits, probs=probs, crit=crit)
-    true_negatives = true_negative(yhat, targets)
-    return specificity(true_negatives, y)
+    true_negatives = is_true_negative(yhat, targets)
+    return specificity(true_negatives, targets)
 
 
 def prec(logits=None, probs=None, targets=None, crit=0.5):
     yhat = yhat_positive(logits=logits, probs=probs, crit=crit)
-    is_true_positives = is_true_positive(yhat, y)
-    is_false_positives = is_false_positive(yhat, y)
+    is_true_positives = is_true_positive(yhat, targets)
+    is_false_positives = is_false_positive(yhat, targets)
     return precision(is_true_positives, is_false_positives)
 
 
